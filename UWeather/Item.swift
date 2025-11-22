c7171a534b5048095c51eb6e84fb5bb5199c7ce0
Rahul Ranjan <rahul.rrixe@gmail.com>
2025-11-22 15:14:19 +0100

    Initial Commit

---
+++ UWeather/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  UWeather
+//
+//  Created by Ranjan, Rahul on 11/22/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
